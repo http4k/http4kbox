939b2641a5f131de60db1585d79d14ab8c20d71f
David Denton <denton.david@gmail.com>
2018-08-05 10:08:56 +0100

    add apache client for the time being as java client does not sign S3 requests properly.

--- local/src/main/kotlin/http4kbox/LocalServer.kt
+++ local/src/main/kotlin/http4kbox/LocalServer.kt
@@ -1,9 +1,7 @@
 package http4kbox
 
 import io.github.konfigur8.Property
-import org.http4k.client.JavaHttpClient
-import org.http4k.core.then
-import org.http4k.filter.DebuggingFilters
+import org.http4k.client.ApacheClient
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 
@@ -11,5 +9,5 @@ val HTTP_PORT = Property.int("HTTP_PORT")
 
 fun main(args: Array<String>) {
     val config = Settings.defaults.withProp(HTTP_PORT, 8000).reify()
-    Http4kBox(config, DebuggingFilters.PrintRequestAndResponse().then(JavaHttpClient())).asServer(Undertow(config[HTTP_PORT])).start().block()
+    Http4kBox(config, ApacheClient()).asServer(Undertow(config[HTTP_PORT])).start().block()
 }
