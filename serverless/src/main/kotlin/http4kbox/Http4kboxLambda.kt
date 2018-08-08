2c98950e6a6f57a3a9e12d26972e5d5c8885a3dd
David Denton <denton.david@gmail.com>
2018-08-08 09:53:26 +0100

    lamdba tweaks and upgrade http4k

--- serverless/src/main/kotlin/http4kbox/Http4kboxLambda.kt
+++ serverless/src/main/kotlin/http4kbox/Http4kboxLambda.kt
@@ -1,24 +1,13 @@
 package http4kbox
 
-import io.github.konfigur8.Property
 import org.http4k.client.JavaHttpClient
-import org.http4k.core.Credentials
 import org.http4k.core.HttpHandler
-import org.http4k.core.then
-import org.http4k.filter.ServerFilters.BasicAuth
 import org.http4k.serverless.AppLoader
 
+
 object Http4kboxLambda : AppLoader {
-    // since we are running in a public environment, add credentials to the app
-    private val CREDENTIALS = Property("CREDENTIALS", String::toCredentials)
-
     override fun invoke(env: Map<String, String>): HttpHandler {
-        System.setProperties(env.toProperties())
-
-        val config = Settings.defaults.requiring(CREDENTIALS).reify()
-
-        return BasicAuth("http4k", config[CREDENTIALS]).then(Http4kBox(config, JavaHttpClient()))
+        env.forEach { System.setProperty(it.key, it.value)}
+        return Http4kBox(Settings.defaults.reify(), JavaHttpClient())
     }
 }
-
-private fun String.toCredentials() = split(":").run { Credentials(get(0), get(1)) }
--- serverless/src/test/kotlin/http4kbox/RunLambda.kt
+++ serverless/src/test/kotlin/http4kbox/RunLambda.kt
@@ -1,6 +1,5 @@
 
 import http4kbox.Http4kboxLambda
-import org.http4k.base64Encode
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
@@ -10,35 +9,21 @@ import org.http4k.serverless.BootstrapAppLoader.HTTP4K_BOOTSTRAP_CLASS
 import org.http4k.serverless.lambda.ApiGatewayProxyRequest
 import org.http4k.serverless.lambda.LambdaFunction
 
-val basicAuth = "http4kbox:http4kbox"
+val defaults = mapOf("S3_REGION" to "us-east-1", "AWS_BUCKET" to "http4kbox")
 
-val settings = mapOf(
-        "CREDENTIALS" to basicAuth,
-        "AWS_CREDENTIALS" to (System.getenv("AWS_CREDENTIALS") ?: "ACCESS_KEY:SECRET_KEY"),
-        "S3_REGION" to "us-east-1",
-        "AWS_BUCKET" to "http4kbox"
-)
-
+/**
+ * Run this main with the AWS_CREDENTIALS environment variable in the format "<ACCESS_KEY>:<SECRET_KEY>"
+ */
 fun main(args: Array<String>) {
-
-    // Launching your Lambda Function locally - by simply providing the operating ENVIRONMENT map as would
-    // be configured on AWS.
-    fun runLambdaLocally() {
-        val app = Http4kboxLambda(settings)
-        val localLambda = app.asServer(SunHttp(8000)).start()
-
-        println(JavaHttpClient()(Request(GET, "http://localhost:8000/")))
-        localLambda.stop()
-    }
 
     // the following code is purely here for demonstration purposes, to explain exactly what is happening at AWS.
     fun runLambdaAsAwsWould() {
-        System.setProperties(settings.toProperties())
+        defaults.forEach { System.setProperty(it.key, it.value) }
+
         val lambda = LambdaFunction(mapOf(HTTP4K_BOOTSTRAP_CLASS to Http4kboxLambda::class.java.name))
         val response = lambda.handle(ApiGatewayProxyRequest().apply {
             path = "/"
             httpMethod = "GET"
-            headers = mapOf("Authorization" to basicAuth.base64Encode())
             queryStringParameters = mapOf()
         })
         println(response.statusCode)
@@ -46,6 +31,18 @@ fun main(args: Array<String>) {
         println(response.body)
     }
 
-    runLambdaLocally()
+    // Launching your Lambda Function locally as a server - by simply providing the operating ENVIRONMENT map as would
+    // be configured on AWS.
+    fun runLambdaLocally() {
+        val app = Http4kboxLambda(defaults)
+        val localLambda = app.asServer(SunHttp(8000)).start()
+
+        println(JavaHttpClient()(Request(GET, "http://localhost:8000/")))
+//        localLambda.stop()
+    }
     runLambdaAsAwsWould()
+
+    runLambdaLocally()
+
+    Thread.currentThread().join()
 }
