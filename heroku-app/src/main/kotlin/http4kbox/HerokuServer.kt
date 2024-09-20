2eed4771dfc6ad66f40c92c1194ddca7bca46cd4
David Denton <denton.david@gmail.com>
2024-09-20 13:09:01 +0100

    Don't use basic auth for heroku now

--- heroku-app/src/main/kotlin/http4kbox/HerokuServer.kt
+++ heroku-app/src/main/kotlin/http4kbox/HerokuServer.kt
@@ -1,26 +1,14 @@
 package http4kbox
 
 import org.http4k.client.HelidonClient
-import org.http4k.config.Environment
-import org.http4k.config.EnvironmentKey
-import org.http4k.core.Credentials
-import org.http4k.core.then
-import org.http4k.filter.ServerFilters.BasicAuth
+import org.http4k.config.Environment.Companion.ENV
 import org.http4k.server.Helidon
 import org.http4k.server.asServer
 import java.time.Clock
 
-// since we are running in a public environment, add credentials to the app
-val BASIC_AUTH_CREDENTIALS = EnvironmentKey.map(String::toCredentials).required("BASIC_AUTH_CREDENTIALS")
-
 fun main(args: Array<String>) {
-    val env = Environment.ENV
-
     val port = if (args.isNotEmpty()) args[0].toInt() else 5000
 
-    BasicAuth("http4k", BASIC_AUTH_CREDENTIALS(env))
-        .then(Http4kBox(env, HelidonClient(), Clock.systemUTC()))
+    Http4kBox(ENV, HelidonClient(), Clock.systemUTC())
         .asServer(Helidon(port)).start().block()
 }
-
-private fun String.toCredentials() = split(":").run { Credentials(get(0), get(1)) }
